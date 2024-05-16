42a9cad5a95417846e25bab2cd201fce7f7cb390
Ann Sargsyan <annsargsyan2707@gmail.com>
2024-05-16 15:08:54 +0400

    testig and debugging

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let items = document.querySelectorAll(".item");
+  let columns = document.querySelectorAll(".column");
 
+  items.forEach(function (item) {
+    item.draggable = "true";
+    item.addEventListener("dragstart", dragStart);
+  });
+
+  columns.forEach(function (column) {
+    column.addEventListener("dragover", dragOver);
+    column.addEventListener("drop", drop);
+  });
+
+  function dragStart(event) {
+    event.dataTransfer.setData("text/plain", event.target.id);
+  }
+
+  function dragOver(event) {
+    event.preventDefault();
+  }
+
+  function drop(event) {
+    event.preventDefault();
+    let data = event.dataTransfer.getData("text/plain");
+    let draggedItem = document.getElementById(data);
+    if (!draggedItem) {
+      console.error("Dragged item not found");
+      return;
+    }
+    let dropTarget = event.currentTarget;
+
+    dropTarget.appendChild(draggedItem);
+  }
+});
