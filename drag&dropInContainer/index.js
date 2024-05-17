d0a596c0509fc1a6c4217be75839e4901f5a19ed
Ann Sargsyan <annsargsyan2707@gmail.com>
2024-05-17 18:53:26 +0400

    for two divs

--- drag&dropInContainer/index.js
+++ drag&dropInContainer/index.js
@@ -1,25 +1,41 @@
 const container = document.getElementById("container");
-const myDiv = document.getElementById("myDiv");
 
 let offsetX, offsetY; //mkniki heravorutyuny div dzax ev verevi pateric
+const myDivs = document.querySelectorAll(".myDiv");
 
-myDiv.addEventListener("mousedown", (e) => {
-  offsetX = e.clientX - myDiv.getBoundingClientRect().left;
-  offsetY = e.clientY - myDiv.getBoundingClientRect().top;
+myDivs.forEach((myDiv) => {
+  myDiv.addEventListener("mousedown", (e) => {
+    offsetX = e.clientX - myDiv.getBoundingClientRect().left;
+    offsetY = e.clientY - myDiv.getBoundingClientRect().top;
 
-  console.log("offX, offy", offsetX, offsetY);
+    console.log("offX, offy", offsetX, offsetY);
 
-  document.addEventListener("mousemove", moveElement);
-  document.addEventListener("mouseup", dropElement);
-  myDiv.style.cursor = "grabbing";
+    document.addEventListener("mousemove", moveElement);
+    document.addEventListener("mouseup", dropElement);
+    myDiv.style.cursor = "grabbing";
+  });
 });
+//const myDiv = document.getElementById("myDiv");
+//let offsetX, offsetY;
+//mkniki heravorutyuny div dzax ev verevi pateric
+
+// myDiv.addEventListener("mousedown", (e) => {
+//   offsetX = e.clientX - myDiv.getBoundingClientRect().left;
+//   offsetY = e.clientY - myDiv.getBoundingClientRect().top;
+
+//   console.log("offX, offy", offsetX, offsetY);
+
+//   document.addEventListener("mousemove", moveElement);
+//   document.addEventListener("mouseup", dropElement);
+//   myDiv.style.cursor = "grabbing";
+// });
 
 function moveElement(e) {
   let newX = e.clientX - offsetX; //containeri u divi dzax pateri mijev heravorutyuny
   let newY = e.clientY - offsetY; //containeri u divi  verevi pateri mijev heravorutyuny
 
   const containerRect = container.getBoundingClientRect();
-  const myDivRect = myDiv.getBoundingClientRect();
+  const myDivRect = e.target.getBoundingClientRect();
 
   if (newX < containerRect.left) {
     newX = containerRect.left;
@@ -32,10 +48,11 @@ function moveElement(e) {
   } else if (newY + myDivRect.height > containerRect.bottom) {
     newY = containerRect.bottom - myDivRect.height;
   }
-
-  myDiv.style.left = `${newX - containerRect.left}px`;
+  //myDiv.style.left = `${newX - containerRect.left}px`;
+  //myDiv.style.top = `${newY - containerRect.top}px`;
+  e.target.style.left = `${newX - containerRect.left}px`;
   console.log(`${newX - containerRect.left}px`);
-  myDiv.style.top = `${newY - containerRect.top}px`;
+  e.target.style.top = `${newY - containerRect.top}px`;
 
   console.log("drop", { newX, newY });
 }
@@ -43,7 +60,6 @@ function moveElement(e) {
 function dropElement() {
   document.removeEventListener("mousemove", moveElement);
   document.removeEventListener("mouseup", dropElement);
-  myDiv.style.cursor = "grab";
 
   console.log("end");
 }
